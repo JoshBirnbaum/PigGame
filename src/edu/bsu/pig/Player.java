ed2623e8b98e873ef04094af73c6f73f872a5c78
joshbirnbaum <jtbirnbaum@bsu.edu>
2021-05-10 13:38:42 -0400

    Add a simple console application. text-based UI.

--- src/edu/bsu/pig/Player.java
+++ src/edu/bsu/pig/Player.java
@@ -1,14 +1,20 @@
 package edu.bsu.pig;
-
 public final class Player {
 
+    private final String name;
     private int score;
-
+    public Player(String name) {
+        this.name = name;
+    }
     public void setScore(int score) {
         this.score = score;
     }
 
     public int getScore() {
         return this.score;
+    }
+
+    public String getName() {
+        return name;
     }
 }
