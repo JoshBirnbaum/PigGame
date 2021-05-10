a77da2c4e7209e6a3643f28080e046a6ace63b04
joshbirnbaum <jtbirnbaum@bsu.edu>
2021-05-10 14:09:34 -0400

    Test score addition with a fixed value die.

--- src/edu/bsu/pig/Turn.java
+++ src/edu/bsu/pig/Turn.java
@@ -1,7 +1,19 @@
 package edu.bsu.pig;
 
 public class Turn {
+    
+    private FixedValueDie die;
+    private int score = 0;
+    
+    public Turn(FixedValueDie die) {
+        this.die = die;
+    }
+
     public int getScore() {
-        return 0;
+        return score;
+    }
+
+    public void roll() {
+        score += die.getValue();
     }
 }
--- test/edu/bsu/pig/TurnTest.java
+++ test/edu/bsu/pig/TurnTest.java
@@ -7,7 +7,14 @@ public class TurnTest {
 
     @Test
     public void getScore_startsAtZero() {
-        Turn turn = new Turn();
+        Turn turn = new Turn(new FixedValueDie(3));
         Assert.assertEquals(0, turn.getScore());
+    }
+
+    @Test
+    public void textRoll_modifiesScoreByRollAmount() {
+        Turn turn = new Turn(new FixedValueDie(3));
+        turn.roll();
+        Assert.assertEquals(3, turn.getScore());
     }
 }
