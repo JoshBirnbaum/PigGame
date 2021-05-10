a89683a62c19301410264560083b4cc86d77225c
joshbirnbaum <jtbirnbaum@bsu.edu>
2021-05-10 15:05:31 -0400

    Change player score when turn is ended.

--- src/edu/bsu/pig/Turn.java
+++ src/edu/bsu/pig/Turn.java
@@ -1,12 +1,14 @@
 package edu.bsu.pig;
 
 public class Turn {
-    
+
+    private Player player;
     private Die die;
     private int score = 0;
     
-    public Turn(Die die) {
+    public Turn(Die die, Player player) {
         this.die = die;
+        this.player = player;
     }
 
     public int getScore() {
@@ -19,5 +21,13 @@ public class Turn {
 
     void setScore(int score) {
         this.score = score;
+    }
+
+    public void end() {
+        this.player.setScore(player.getScore() + this.score);
+    }
+
+    public Player getPlayer() {
+        return player;
     }
 }
